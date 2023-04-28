486a51d275cac834042f834df1c30b7e215a21e2
lpata1 <68364659+lpata1@users.noreply.github.com>
2023-04-28 00:24:24 -0400

    Project Done!

--- flask_webpage_screen.py
+++ flask_webpage_screen.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import python_stock_functions
+from python_stock_functions import find_FV, find_NPER, find_PMT, find_PV, find_RATE, top_10, bottom_10, company_information, compare
+import requests
+from api_keys import stock_ticker_api, finnhub_key
 # from python_stock_functions import
 # Add to the imports when the functions are done
 
@@ -14,33 +18,135 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/button1')
+@app.route('/button1') #This is the Financial Calculator
 def button1():
     return render_template('button1.html')
 
-@app.route('/button2')
+@app.post('/submit')
+def find_solution():
+    """"""
+    answer = 'None'
+    Found_Answer = 'None'
+    if request.form['NPER'] == '?':
+        RATE = float(request.form['RATE'])
+        PV = float(request.form['PV'])
+        PMT = float(request.form['PMT'])
+        FV = float(request.form['FV'])
+        answer = find_NPER(RATE, PV, PMT, FV)
+        Found_Answer = 'NPER'
+    elif request.form['RATE'] == '?':
+        NPER = float(request.form['NPER'])
+        PV = float(request.form['PV'])
+        PMT = float(request.form['PMT'])
+        FV = float(request.form['FV'])
+        answer = find_RATE(NPER, PV, PMT, FV)
+        Found_Answer = 'RATE'
+    elif request.form['PV'] == '?':
+        NPER = float(request.form['NPER'])
+        RATE = float(request.form['RATE'])
+        PMT = float(request.form['PMT'])
+        FV = float(request.form['FV'])
+        answer = find_PV(NPER, RATE, PMT, FV)
+        Found_Answer = 'PV'
+    elif request.form['PMT'] == '?':
+        NPER = float(request.form['NPER'])
+        RATE = float(request.form['RATE'])
+        PV = float(request.form['PV'])
+        FV = float(request.form['FV'])
+        answer = find_PMT(NPER, RATE, PV, FV)
+        Found_Answer = 'PMT'
+    elif request.form['FV'] == '?':
+        NPER = float(request.form['NPER'])
+        RATE = float(request.form['RATE'])
+        PV = float(request.form['PV'])
+        PMT = float(request.form['PMT'])
+        answer = find_FV(NPER, RATE, PV, PMT)
+        Found_Answer = 'FV'
+    else:
+        NPER = float(request.form['NPER'])
+        RATE = float(request.form['RATE'])
+        PV = float(request.form['PV'])
+        PMT = float(request.form['PMT'])
+        FV = float(request.form['FV'])
+    return render_template('Fin_Cal_answer.html', answer = answer, Found_Answer = Found_Answer) #, NPER=NPER, RATE=RATE, PV=PV, PMT=PMT, FV=FV )
+
+
+@app.route('/button2') #This Finds the best and wrost performing stock of the day
 def button2():
     return render_template('button2.html')
 
-@app.route('/button3')
-def button3():
-    return render_template('button3.html')
+@app.route('/best')
+def best():
+    top_10()
+    return render_template('button2.html')
 
-@app.route('/button4')
+@app.route('/worst')
+def worst():
+    bottom_10()
+    return render_template('button2.html')
+
+
+"""Stock Ticker was attempted multiple differnet ways and was not able to be completed sadly"""
+# @app.route('/button3') #This is the Stock Ticker 
+# def button3():
+#     api_key_ticker = stock_ticker_api()
+#     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=SPX&interval=1min&apikey={api_key_ticker}'
+#     response = requests.get(url)
+#     data = response.json()
+#     ticker = data['Time Series (1min)']
+#     return render_template('button3.html', ticker=ticker)
+
+@app.route('/button4') #This is the simplified mortgage calculator
 def button4():
     return render_template('button4.html')
 
-@app.route('/button5')
+@app.post('/mortgage')
+def find_mortgage_rate_or_payment():
+    answer = "None"
+    Found_Answer = 'None'
+    if request.form['RATE'] == '?':
+        NPER = 12 * float(request.form['NPER'])
+        PV = float(request.form['PV'])
+        PMT = float(request.form['PMT'])
+        answer = find_RATE(NPER, PV, PMT, 0)
+        Found_Answer = 'Monthly Rate Required'
+    elif request.form['PMT'] == '?':
+        NPER = 12 * float(request.form['NPER'])
+        RATE = float(request.form['RATE']) / 12
+        PV = float(request.form['PV'])
+        answer = find_PMT(NPER, RATE, PV, 0)
+        Found_Answer = 'Payment Required'
+    else:
+        # NPER = float(request.form['NPER'])
+        # RATE = float(request.form['RATE'])
+        # PV = float(request.form['PV'])
+        # PMT = float(request.form['PMT'])
+        # FV = float(request.form['FV'])
+        None
+    return render_template('mortgage_ans.html', answer = answer, Found_Answer = Found_Answer)
+
+
+@app.route('/button5') #This is the Stock Ticker Analysis
 def button5():
     return render_template('button5.html')
 
-@app.route('/button6')
+@app.post('/Ticker')
+def Ticker():
+    Ticker = request.form['Ticker']
+    Name, Sector, Industry, CurrentPrice, Recommendation, Business_Summary = company_information(Ticker)
+    return render_template('Ticker.html', Name=Name, Sector=Sector, Industry=Industry, CurrentPrice=CurrentPrice, Recommendation=Recommendation, Business_Summary=Business_Summary)
+
+@app.route('/button6') # This is the two stock comparative analysis
 def button6():
     return render_template('button6.html')
 
+@app.post('/Compare')
+def comparison():
+    Ticker1=request.form['Ticker1']
+    Ticker2=request.form['Ticker2']
+    currentPrice1, ebitdaMargins1, enterpriseValue1, forwardEps1, forwardPE1, pricetoBook1, revenuePerShare1 = compare(Ticker1)
+    currentPrice2, ebitdaMargins2, enterpriseValue2, forwardEps2, forwardPE2, pricetoBook2, revenuePerShare2 = compare(Ticker2)
+    return render_template('compare.html',currentPrice1=currentPrice1, currentPrice2=currentPrice2, ebitdaMargins1=ebitdaMargins1, ebitdaMargins2=ebitdaMargins2, enterpriseValue1=enterpriseValue1, enterpriseValue2=enterpriseValue2, forwardEps1=forwardEps1, forwardEps2=forwardEps2, forwardPE1=forwardPE1, forwardPE2=forwardPE2, pricetoBook1=pricetoBook1, pricetoBook2=pricetoBook2, revenuePerShare1=revenuePerShare1, revenuePerShare2=revenuePerShare2, Ticker1=Ticker1, Ticker2=Ticker2)
+
 if __name__=='__main__':
     app.run(debug=True)
-
-""""""
-#Have THe Button Pathways ready
-""""""
--- python_stock_functions.py
+++ python_stock_functions.py
@@ -4,12 +4,16 @@ This is all of the Python Functions that I will be using and testing for the pro
 """
 ###
 import math
-
+from api_keys import stock_ticker_api, finnhub_key
+import requests
+import yfinance as yf
+import pprint
 
 def find_NPER(RATE, PV, PMT, FV=0):
     """
     This funntion finds the amount of periods when given RATE, PV, PMT, and FV
     """
+    PV = -PV
     if RATE == 0:
         return -(PV+FV) / PMT
     else:
@@ -49,9 +53,62 @@ def find_FV(NPER=0, RATE=0, PV=0, PMT=0):
     """
     This fucntion finds future/face value when given NPER, RATE, PV, and PMT
     """
-    FV = PV*(1 + RATE/100) ** NPER + PMT * \
-        ((1+RATE/100)**NPER-1) * (1+RATE/100) ** -1
+    PV=-PV
+    FV = PV*(1 + RATE/100.00) ** NPER + PMT * \
+        ((1+RATE/100.00)**NPER-1) * (1+RATE/100.00) ** -1
     return FV
+
+
+import webbrowser
+APIKEY_finnhub = finnhub_key()
+def top_10(): #This was orignally supposed to be an api of data but it wouldn't work, same with the worst performing stocks data
+    """ This function opens the yahoo finance web page of the best performing stocks of the day """
+    url = f"https://finance.yahoo.com/gainers/"
+    webbrowser.open(url)
+    # response = requests.get(url)
+    # data = response.json()
+    # top_10 = []
+    # for stock in data:
+    #     symbol = stock['symbol']
+    #     name = stock['description']
+    #     change_percent = stock['changePercent']
+    #     top_10.append({'symbol': symbol, 'name':name, 'change_percent': change_percent})
+
+    # return top_10
+
+def bottom_10():
+    """ This function opens the yahoo finance web page of the worst performing stocks of the day """
+    url = f"https://finance.yahoo.com/losers/"
+    webbrowser.open(url)
+    # response = requests.get(url)
+    # data = response.json()
+    # bottom_10 = data[:10]
+
+    # return bottom_10
+
+def company_information(Ticker):
+    """This function takes a stock's ticker and then gives back information about the company"""
+    Ticker = yf.Ticker(Ticker)
+    Name = Ticker.info['longName']
+    Sector = Ticker.info['sector']
+    Industry = Ticker.info['industry']
+    CurrentPrice = Ticker.info['currentPrice']
+    Recommendation = Ticker.info['recommendationKey']
+    Business_Summary = Ticker.info['longBusinessSummary']
+    return Name, Sector, Industry, CurrentPrice, Recommendation, Business_Summary
+
+def compare(Ticker):
+    """This function gives the values from the API that are needed for the comparison"""
+    Ticker = yf.Ticker(Ticker)
+    currentPrice = Ticker.info['currentPrice']
+    ebitdaMargins = Ticker.info['ebitdaMargins']
+    enterpriseValue = Ticker.info['enterpriseValue']
+    forwardEps = Ticker.info['forwardEps']
+    forwardPE = Ticker.info['forwardPE']
+    priceToBook = Ticker.info['priceToBook']
+    revenuePerShare = Ticker.info['revenuePerShare']
+    return currentPrice, ebitdaMargins, enterpriseValue, forwardEps, forwardPE, priceToBook, revenuePerShare
+
 
 
 def main():
@@ -61,6 +118,10 @@ def main():
     # print(find_PV(10, .05, 50, 0))
     # print(find_PMT(10, .05, 1000, 0))
     # print(find_FV(10, .05, 100, 50))
+    # print(top_10())
+    # print(bottom_10())
+    # print(company_information('MSFT'))
+
 
 
 if __name__ == '__main__':
